7e3b3a8f084485a27854b73bc025a08dad46ed37
TrevHeath <trevheath7@gmail.com>
2020-01-19 17:54:36 -0800

    start admin screen

---
+++ src/routes/admin/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import buildGraphQLProvider from "ra-data-graphql";
+import { Admin, Resource, Delete } from "react-admin";
+
+import buildQuery from "./buildQuery"; // see Specify your queries and mutations section below
+import { PostCreate, PostEdit, PostList } from "../components/admin/posts";
+
+const Admin = () => {
+  return <div>admin</div>;
+};
+
+export default App;
