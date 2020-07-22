0c4ca6945651b9e8ed5a39badac4922162cf5894
TrevHeath <trevheath7@gmail.com>
2020-07-22 13:44:05 -0700

    fix isempty check

--- src/routes/admin/EditAgency.js
+++ src/routes/admin/EditAgency.js
@@ -110,7 +110,7 @@ export default function UserManagement() {
 
   const onUpdate = async (values) => {
     try {
-      if (!isEmpty(values)) {
+      if (isEmpty(values)) {
         add({ content: "No updates made.", variant: "error" });
         return;
       }
